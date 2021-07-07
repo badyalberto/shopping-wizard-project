4ba82c29d5f9031e037d6b56500faecb27867805
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-07-07 13:16:57 +0200

    JS Shipping

--- js/script.js
+++ js/script.js
@@ -3,22 +3,50 @@ let firstBuy = document.getElementById("first-buy");
 firstBuy.addEventListener("click", filling);
 
 function filling() {
-  document.getElementById("finish-name").textContent =
-    document.getElementById("video-name").textContent;
-  document.getElementById("finish-platform").textContent +=
+  document.getElementById("finish-name").innerHTML =
+    "<b>" + document.getElementById("video-name").innerHTML + "</b>";
+  document.getElementById("finish-platform").innerHTML +=
     " " +
+    "<b>" +
     document.getElementById("video-select")[
       document.getElementById("video-select").selectedIndex
-    ].textContent;
-  document.getElementById("finish-price").textContent +=
-    " " + document.getElementById("video-price").textContent;
+    ].textContent +
+    "</b>";
+  document.getElementById("finish-price").innerHTML +=
+    " " + "<b>" + document.getElementById("video-price").innerHTML + "</b>";
 }
 
 let shipBtn = document.getElementById("next-shipping");
-shipBtn.addEventListener("click", fillingShip);
-function fillingShip (){
-    if (document.querySelector(".select-option-shipping") == "")
+shipBtn.addEventListener("click", filling2);
+function filling2(evt) {
+  evt.preventDefault();
+  let complex = document.querySelector(".shipping-type");
+  console.log(complex);
+  let inp = complex.querySelectorAll('input[name = "shipping"]');
+  console.log(inp);
+  let sel = "";
+  for (let input of inp) {
+    if (input.checked) {
+      sel = input.value;
+    }
+  }
+  let finishShipping = document.getElementById("finish-shipping");
+
+  switch (sel) {
+    case "free":
+      finishShipping.innerHTML += " " + "<b>" + "0€" + "</b>";
+      break;
+    case "extra":
+      finishShipping.innerHTML += " " + "<b>" + "4.99€" + "</b>";
+      break;
+    case "premium":
+      finishShipping.innerHTML += " " + "<b>" + "9.99€" + "</b>";
+      break;
+    default:
+      break;
+  }
 }
+
 // Finished Div Page
 let termsCheck = document.getElementById("cbox1");
 let finalButton = document.getElementById("buy-now");
